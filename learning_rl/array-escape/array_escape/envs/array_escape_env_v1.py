73bb51de8cd34aa5e83709b332a9783a8f63b0d7
garylvov <67614381+garylvov@users.noreply.github.com>
2021-08-26 15:59:30 -0400

    Update array_escape_env_v1.py

--- learning_rl/array-escape/array_escape/envs/array_escape_env_v1.py
+++ learning_rl/array-escape/array_escape/envs/array_escape_env_v1.py
@@ -1,92 +1,159 @@
-import array_escape
+import time
+import random
 import gym
-import numpy as np 
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.optimizers import Adam
-from rl.agents import DQNAgent
-from rl.policy import BoltzmannQPolicy
-from rl.memory import SequentialMemory
+import numpy as np
+from gym import spaces, error
+from gym.utils import seeding
+from gym.envs.classic_control import rendering
 
-class DQN():
-    def __init__(self, policy=BoltzmannQPolicy()):
-        self.create_env()
-        self.create_model()
-        self.create_memory(policy)
-
-    def create_env(self):
-        self.env = gym.make('array_escape-v1')
-        self.states = self.env.observation_space.shape[0]
-        self.actions = self.env.action_space.n
-        
-    def create_model(self):
-        self.model = Sequential()
-        self.model.add(Flatten(input_shape=(1, self.states, 2)))
-        self.model.add(Dense(60, activation='relu'))
-        self.model.add(Dense(80, activation='relu'))
-        self.model.add(Dense(self.actions, activation='linear'))
-        self.model.summary()
-
-    def create_memory(self, policy):
-        self.policy = policy
-        self.memory = SequentialMemory(limit=5000, window_length=1)
-        self.dqn = DQNAgent(model=self.model, memory=self.memory, policy = self.policy,
-                            nb_actions = self.actions, nb_steps_warmup=10, target_model_update=1e-2)
-        self.dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
-
-    def train_model(self):
-        print("\n\n\n\n\n\n\n\n")
-        history = self.dqn.fit(self.env, nb_steps=1000000, visualize=False, verbose=1)
-        
-        self.scores = (history.history['episode_reward'])
-        self.episodes = [None] * len(self.scores)
-
-        scores = self.dqn.test(self.env, nb_episodes = 100, visualize=False)
-        print(np.mean(scores.history['episode_reward']))
-
-        _ = self.dqn.test(self.env, nb_episodes=5, visualize=True)
-        self.dqn.save_weights('dqn_weights.h5f', overwrite=True)
-        self.env.close()
-
-    def plot(self):
-        print("\n\n\n\n\n\n\n\n")
-        plt.xlabel('Episode')
-        plt.ylabel('Score')
-
-        for x in range (len(self.scores)):
-            self.episodes[x] = x
-        scores_line = plt.plot(self.episodes, self.scores)
-
-        plt.setp(scores_line,'color', 'r', 'linewidth', 2.0)
-        plt.show()
-
-    def load_weights(self):
-        self.dqn.load_weights('dqn_weights.h5f')
-        _ = self.dqn.test(self.env, nb_episodes=10, visualize=True)
-        self.env.close() 
-
-    def large_test(self):
-        test = self.dqn.test(self.env, nb_episodes=250, visualize=True)
-        print(test.history.keys())
-        self.scores = (test.history['episode_reward'])
-        self.episodes = (test.history['nb_steps'])
-        print(len(self.scores))
-        print(len(self.episodes))
-        
-    def reset_model(self):
-        del self.model
-        del self.dqn
-        del self.env
-        self.create_env()
-        self.create_model()
-        self.create_memory()
-
-if __name__=='__main__':
-    model = DQN()
-    model.train_model()
-    model.plot()
+class ArrayEscapeEnvV1(gym.Env):
+    metadata = {'render.modes': ['human']}
     
-    loaded_weights_model = DQN()
-    loaded_weights_model.load_weights()
-    loaded_weights_model.large_test()
+    def __init__(self):
+        self.grid_size = 10 #grid is square, minumum size of 3
+        self.max_dist = ((self.grid_size**2) * 2) ** .5
+        self.max_steps = self.grid_size * 1.5
+        self.mine_num = int(self.grid_size * 1.75) + 1
+        self.items_depth = self.mine_num + 3
+        self.last_item = [0,0]
+        self.items = None
+        self.state = None
+        self.viewer = None
+        #X 4 X
+        #3 0 1
+        #X 2 X
+        #.................0..1..2..3..4
+        self.movement = [[0, 1, 0,-1], 
+                         [1, 0, 1, 0]]
+        
+        high = np.array(self._create_populated_array(0))
+        low = np.array(self._create_populated_array(self.grid_size -1))
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low, high, dtype=np.int32)
+        self.seed()
+        
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def reset(self):
+        self.items = self._create_populated_items_array(self.items_depth)
+        self.state = self.items
+        self._reset_render()
+        self.steps_elapsed_in_ep = 0
+        return np.array(self.state)
+
+    def step(self, action):
+        self.steps_elapsed_in_ep +=1
+        self.last_item[0] = self.items[0][0]
+        self.last_item[1] = self.items[0][1]
+
+        self.items[0][0] = self.items[1][0]
+        self.items[0][1] = self.items[1][1]
+
+        dist = (((self.items[1][0] - self.items[2][0])**2 + (self.items[1][1] - self.items[2][1])**2)**.5)
+
+        new_column = self.items[1][0] + self.movement[0][action]
+        new_row = self.items[1][1] + self.movement[1][action]
+
+        done = bool(
+            new_column < 0
+            or new_row < 0
+            or new_column > self.grid_size -1
+            or new_row > self.grid_size -1
+            or self._check_mines()
+            #or self.steps_elapsed_in_ep > self.max_steps
+        )
+
+        if not done: 
+            self.items[1][0] += self.movement[0][action]
+            self.items[1][1] += self.movement[1][action]
+
+            if(self.last_item[0] == self.items[1][0] and self.last_item[1] == self.items[1][1]):
+                #print("REPEAT")
+                reward = self.grid_size * -3
+                done = True
+
+            self.state = self.items
+            reward = 1 #* -dist
+        else:
+            if dist != 0:
+                reward = self.grid_size * -3
+            else:
+                reward = self.grid_size * 10
+        
+        return np.array(self.state), reward, done, {}
+
+    def render(self, mode='human'):
+        
+        self.screen_side = 800
+        self.line_dist = self.screen_side / self.grid_size
+
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(self.screen_side, self.screen_side)
+        time.sleep(.15)
+        for y in range(self.items_depth):
+            box = rendering.FilledPolygon([
+                (self.items[y][0] * self.line_dist, self.items[y][1] * self.line_dist), 
+                ((self.items[y][0] * self.line_dist) + self.line_dist, self.items[y][1] * self.line_dist), 
+                ((self.items[y][0] * self.line_dist) + self.line_dist, (self.items[y][1] * self.line_dist) + self.line_dist), 
+                ((self.items[y][0] * self.line_dist), (self.items[y][1] * self.line_dist) + self.line_dist)])
+            if(y == 0):
+                box.set_color(0, 50, 50)
+            if(y == 1):
+                box.set_color(0, 0, 255)
+            if(y == 2):
+                box.set_color(0, 255, 0)
+            if(y > 2):
+                    box.set_color(255, 0, 0)
+            self.viewer.add_geom(box)
+        return self.viewer.render(return_rgb_array=mode == "rgb_array")
+
+    def _create_populated_array(self, num):
+        array = np.empty((self.items_depth, 2)).astype(np.int32)
+        for y in range(self.items_depth):
+            for x in range(2):
+                array[y][x] = num
+        return array 
+    
+    def _create_populated_items_array(self, num):
+        array = np.empty((num, 2)).astype(np.int32)
+        for y in range(1, num):
+            for x in range(2):
+                array[y][x] = self.np_random.randint(0, (self.grid_size -1))
+        
+        #ensure that moving square doesn't start on a mine
+        for y in range(2, num):
+            if(array[1][0] == array[y][0] or array[2][0] == array[y][0]):
+                array[y][0] = self.np_random.randint(0, (self.grid_size -1))
+            elif(array[1][1] == array[y][0] or array[2][1] == array[y][0]):
+                array[y][1] = self.np_random.randint(0, (self.grid_size -1))
+        return array
+
+    def _check_mines(self):
+        collided = False
+        for x in range(2, self.items_depth):
+            if(self.items[1][0] == self.items[x][0] and self.items[1][1] == self.items[x][1]):
+                collided = True
+        return collided
+        
+    def _draw_grid(self):
+        for x in range(self.grid_size):
+            x_line = rendering.Line((0, (x * self.line_dist)),(self.screen_side, (x * self.line_dist)))
+            y_line = rendering.Line(((x * self.line_dist), 0),((x * self.line_dist), self.screen_side))
+            x_line.set_color(0, 0, 0)
+            y_line.set_color(0, 0, 0)
+            self.viewer.add_geom(x_line)
+            self.viewer.add_geom(y_line)
+
+    def _reset_render(self):
+        if self.viewer is not None:
+            box = rendering.FilledPolygon([
+                (0, 0), 
+                (0, self.screen_side), 
+                (self.screen_side, self.screen_side), 
+                (self.screen_side, 0)])
+
+            box.set_color(255, 255, 255)
+            self.viewer.add_geom(box)
+            self.viewer.render()
